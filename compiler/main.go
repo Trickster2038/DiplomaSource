df1bec0a0c501b1f19507db63f29f2c4db32c7f0
Astakhov Sergey <fzastahov@gmail.com>
2022-12-14 17:28:02 +0300

    Compiler service - proto

--- compiler/main.go
+++ compiler/main.go
@@ -4,7 +4,6 @@ import (
     "fmt"
     "os/exec"
     "runtime"
-    //"strings"
     "os"
     "net/http"
     "encoding/json"
@@ -19,74 +18,118 @@ type SourceFiles struct {
     Tb_src string `json:"tb_src"`
 }
 
-func create_or_update(user_id string, level_id string, device_src  string, tb_src string) {
-    os.MkdirAll((user_id + "/" + level_id), os.ModePerm)
-    f, _ := os.Create((user_id + "/" + level_id + "/device.v"))
-    f.WriteString(device_src)
-    f, _ = os.Create((user_id + "/" + level_id + "/tb.v"))
-    f.WriteString(tb_src)
+type ResponseFrame struct {
+    Status_str string `json:"status_str"`
+    Status_code int `json:"status_code"`
+    Message string `json:"message,omitempty"`
+    Value_change_dump string `json:"value_change_dump,omitempty"`
 }
 
-func compile_and_visualise(user_id string, level_id string) {
+func create_or_update(user_id string, level_id string, device_src  string, tb_src string) int {
+    os.MkdirAll((user_id + "/" + level_id), 0777)
+    f, err1 := os.Create((user_id + "/" + level_id + "/device.v"))
+    _, err2 := f.WriteString(device_src)
+    f, err3 := os.Create((user_id + "/" + level_id + "/tb.v"))
+    _, err4 := f.WriteString(tb_src)
+    if ((err1 != nil) || (err2 != nil) || (err3 != nil) || (err4 != nil)){
+        return 1
+    }
+    return 0
+}
+
+func compile_and_visualise(user_id string, level_id string) int {
     device_path := user_id + "/" + level_id + "/device.v"
     tb_path := user_id + "/" + level_id + "/tb.v"
     out_path := user_id + "/" + level_id + "/device"
 
     _, err := exec.Command("bash", "-c", ("iverilog -o " + out_path + " " + device_path + " " + tb_path)).Output()
     if err != nil {
-        fmt.Printf("%s", err)
+        return 1
     }
 
     _, err = exec.Command("bash", "-c", ("vvp " + out_path)).Output()
     if err != nil {
-        fmt.Printf("%s", err)
+        return 2
     }
-    
+
+    return 0
 }
 
 func build(w http.ResponseWriter, req *http.Request) {
-    // for name, headers := range req.Header {
-    //     for _, h := range headers {
-    //         fmt.Fprintf(w, "%v: %v\n", name, h)
-    //     }
-    // }
+    var response ResponseFrame
 
     reqBody, _ := ioutil.ReadAll(req.Body)
-    var post SourceFiles
-    json.Unmarshal(reqBody, &post)
-    // fmt.Println(string(reqBody))
-    // post = &SourceFiles{"a","b","c","d"}
+    var dataFrame SourceFiles
 
-    json.NewEncoder(w).Encode(post)
-
-    newData, err := json.Marshal(post)
-    if err != nil {
-        fmt.Println(err)
-    } else {
-        fmt.Println(string(newData))
+    err := json.Unmarshal(reqBody, &dataFrame)
+    if err != nil { 
+        defer func() {
+            if r := recover(); r != nil {
+                response.Status_str = "error"
+                response.Status_code = 400
+                response.Message = err.Error()
+                w.WriteHeader(response.Status_code)
+                json.NewEncoder(w).Encode(response)
+            }
+        }()
+        panic("JSON parsing error")
     }
 
-    //create_or_update(user_id, level_id, device_src, tb_src)
-    //compile_and_visualise(user, level)
+    // FIXME: file is always locked
+    defer func() {
+        if r := recover(); r != nil {
+            exec.Command(dataFrame.User_id + "/" + dataFrame.Level_id)
+            os.RemoveAll(dataFrame.User_id + "/" + dataFrame.Level_id)
+        }
+    }()
+
+    err_int := create_or_update(dataFrame.User_id, dataFrame.Level_id, 
+        dataFrame.Device_src, dataFrame.Tb_src)
+        if err_int != 0 { 
+            defer func() {
+                if r := recover(); r != nil {
+                    response.Status_str = "error"
+                    response.Status_code = 400
+                    response.Message = "writing files error"
+                    w.WriteHeader(response.Status_code)
+                    json.NewEncoder(w).Encode(response)
+                }
+            }()
+            panic("Writing files error")
+        }
+
+    err_int = compile_and_visualise(dataFrame.User_id, dataFrame.Level_id)
+    if err_int != 0 { 
+        defer func() {
+            if r := recover(); r != nil {
+                response.Status_str = "error"
+                response.Status_code = 400
+                if(err_int == 1){
+                    response.Message = "synthethis error"
+                } else {
+                    response.Message = "simulation error"
+                }
+                w.WriteHeader(response.Status_code)
+                json.NewEncoder(w).Encode(response)
+            }
+        }()
+        panic("Simulation error")
+    }
+
+    value_change_dump, _ := os.ReadFile("adder.vcd") 
+
+    response.Status_str = "ok"
+    response.Status_code = 200
+    response.Message = "compiled successfully"
+    response.Value_change_dump = string(value_change_dump)
+    w.WriteHeader(response.Status_code)
+    json.NewEncoder(w).Encode(response)
 }
 
 func main() {
     if runtime.GOOS == "windows" {
         fmt.Println("Can't Execute this on a windows machine")
     } else {
-        //execute2()
-        f, _ := os.ReadFile("adder.v")
-        device_src := string(f)
-
-        f, _ = os.ReadFile("adder_tb.v")
-        tb_src := string(f)
-
-        user := "user1"
-        level := "level1"
-
-        
-        create_or_update(user, level, device_src, tb_src)
-        compile_and_visualise(user, level)
 
         fmt.Println("Server start")
         r := mux.NewRouter().StrictSlash(true)
