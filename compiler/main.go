8577cc1a9145ee34641868ac5414cc0e2676f311
Astakhov Sergey <fzastahov@gmail.com>
2022-12-14 11:53:07 +0300

    Failing 255

--- compiler/main.go
+++ compiler/main.go
@@ -6,37 +6,8 @@ import (
     "runtime"
     //"strings"
     "os"
+    //"net/http"
 )
-
-func execute() {
-    out, err := exec.Command("screenfetch").Output()
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-    fmt.Println("Command Successfully Executed")
-    output := string(out[:])
-    fmt.Println(output)
-}
-
-func execute2() {
-    out, err := exec.Command("bash", "-c", "iverilog -o adder adder_tb.v adder.v").Output()
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-    fmt.Println("Command Successfully Executed")
-    output := string(out[:])
-    fmt.Println(output)
-
-    // =====
-
-    out, err = exec.Command("bash", "-c", "vvp adder").Output()
-    if err != nil {
-        fmt.Printf("%s", err)
-    }
-    fmt.Println("Command Successfully Executed")
-    output = string(out[:])
-    fmt.Println(output)
-}
 
 func create_or_update(user_id string, level_id string, device_src  string, tb_src string) {
     os.MkdirAll((user_id + "/" + level_id), os.ModePerm)
@@ -50,11 +21,31 @@ func compile_and_visualise(user_id string, level_id string) {
     device_path := user_id + "/" + level_id + "/device.v"
     tb_path := user_id + "/" + level_id + "/tb.v"
     out_path := user_id + "/" + level_id + "/device"
-    _, err := exec.Command("bash", "-c", ("iverilog -o  " + out_path + " " + device_path + " " + tb_path)).Output()
+
+    fmt.Printf("%s", "\np1\n")
+    _, err := exec.Command("bash", "-c", ("iverilog -o " + out_path + " " + device_path + " " + tb_path)).Output()
+    //_, err := exec.Command("bash", "-c", ("iverilog -o  " + out_path + " " + device_path + " " + tb_path)).Output()
     if err != nil {
         fmt.Printf("%s", err)
     }
+    fmt.Printf("%s", "\np2\n")
+
+    _, err = exec.Command("bash", "-c", ("vvp " + out_path)).Output()
+    if err != nil {
+        fmt.Printf("%s", err)
+    }
+    
 }
+
+// func build(w http.ResponseWriter, req *http.Request) {
+//     for name, headers := range req.Header {
+//         for _, h := range headers {
+//             fmt.Fprintf(w, "%v: %v\n", name, h)
+//         }
+//     }
+//     //create_or_update(user_id, level_id, device_src, tb_src)
+//     //compile_and_visualise(user, level)
+// }
 
 func main() {
     if runtime.GOOS == "windows" {
@@ -67,7 +58,16 @@ func main() {
         f, _ = os.ReadFile("adder_tb.v")
         tb_src := string(f)
 
-        create_or_update("a", "b", device_src, tb_src)
-        compile_and_visualise("a", "b")
+        user := "user1"
+        level := "level1"
+
+        
+        create_or_update(user, level, device_src, tb_src)
+        compile_and_visualise(user, level)
+        // fmt.Printf("%s", "gg")
+
+        //http.HandleFunc("/build", build)
+
+        //http.ListenAndServe(":8090", nil)
     }
 }
