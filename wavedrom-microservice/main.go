452b7d1259cad472572d6dfd87b3b878035d0c69
Astakhov Sergey <fzastahov@gmail.com>
2022-12-27 23:07:25 +0300

    Wavedrom in work

--- wavedrom-microservice/main.go
+++ wavedrom-microservice/main.go
@@ -9,12 +9,17 @@ import (
 type VCD_obj struct {
 	Children []struct {
 		Name string `json:"name"`
-		// Data []struct {
-
-		// } `json:"data"`
+		Type struct {
+			Name  string `json:"name"`
+			Width int    `json:"width"`
+		} `json:"type"`
+		Data []interface{} `json:"data"`
 	} `json:"children"`
-	// Children []byte `json:"children"`
-	Gg string `json:"gg"`
+}
+
+type SingleValueDump struct {
+	Time  int
+	Value string
 }
 
 func main() {
@@ -23,14 +28,13 @@ func main() {
 		fmt.Print(err)
 	}
 
-	// fmt.Println(b) // print the content as 'bytes'
-
-	// s := string(b) // convert content to a 'string'
-
-	// fmt.Println(s) // print the content as a 'string'
-
 	var VcdFrame VCD_obj
 	json.Unmarshal(b, &VcdFrame)
 
-	fmt.Println(string(VcdFrame.Children[0].Name))
+	fmt.Println("Name: ", string(VcdFrame.Children[0].Name))
+	fmt.Println("Type: ", string(VcdFrame.Children[0].Type.Name))
+	fmt.Println("Width: ", int(VcdFrame.Children[0].Type.Width))
+	s := VcdFrame.Children[0].Data[1].([]interface{})
+	fmt.Println("Time: ", s[0])
+	fmt.Println("Value: ", s[1])
 }
