2c80ff0176fa2baae6c605e014038b3cbbfa5145
Astakhov Sergey <fzastahov@gmail.com>
2023-01-11 13:15:26 +0300

    Levels CRUD proto

--- crud-microservice/main.go
+++ crud-microservice/main.go
@@ -1,6 +1,9 @@
 package main
 
-// TODO: All microservice =)
+// TODO:
+// - All microservice =)
+// - read_all() for levels_brief
+// - read levels_brief & levels_data (or just call .read(id) twice?)
 
 import (
 	"fmt"
@@ -35,7 +38,7 @@ func (level_brief LevelsBrief) create() {
 	_, err := db.Query("UPDATE LevelsBrief SET seqnum = seqnum + 1 WHERE seqnum >= ?",
 		level_brief.Seqnum)
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		panic(err.Error())
 	}
 
 	_, err = db.Query("INSERT INTO LevelsBrief (level_type, seqnum, cost, is_active, name, brief) VALUES (?, ?, ?, ?, ?, ?)",
@@ -46,7 +49,7 @@ func (level_brief LevelsBrief) create() {
 		level_brief.Name,
 		level_brief.Brief)
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		panic(err.Error())
 	}
 
 	defer db.Close()
@@ -64,10 +67,75 @@ func (level_brief *LevelsBrief) read(id int) {
 			&level_brief.Name,
 			&level_brief.Brief)
 	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
+		panic(err.Error())
 	}
 
 	defer db.Close()
+}
+
+func (level_brief LevelsBrief) update() {
+	db := connect_db()
+
+	var old_level_brief LevelsBrief
+
+	old_level_brief.read(level_brief.ID) // can call panic
+
+	if level_brief.Seqnum > old_level_brief.Seqnum {
+		_, err := db.Query("UPDATE LevelsBrief SET seqnum = seqnum - 1 WHERE seqnum > ? AND seqnum < ?",
+			old_level_brief.Seqnum, level_brief.Seqnum)
+		if err != nil {
+			panic(err.Error())
+		}
+	} else if level_brief.Seqnum < old_level_brief.Seqnum {
+		_, err := db.Query("UPDATE LevelsBrief SET seqnum = seqnum + 1 WHERE seqnum > ? AND seqnum < ?",
+			level_brief.Seqnum, old_level_brief.Seqnum)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	_, err := db.Query("UPDATE LevelsBrief SET "+
+		"level_type = ?, "+
+		"seqnum = ?, "+
+		"cost = ?, "+
+		"is_active = ?, "+
+		"name = ?, "+
+		"brief = ? "+
+		"WHERE id = ?",
+		level_brief.Level_type,
+		level_brief.Seqnum,
+		level_brief.Cost,
+		level_brief.Is_active,
+		level_brief.Name,
+		level_brief.Brief,
+		level_brief.ID)
+	if err != nil {
+		panic(err.Error())
+	}
+
+}
+
+// setting is_active = FALSE
+func (level_brief LevelsBrief) delete() {
+	db := connect_db()
+
+	level_brief.read(level_brief.ID) // can call panic
+	if level_brief.Is_active {
+		_, err := db.Query("UPDATE LevelsBrief SET seqnum = seqnum - 1 WHERE seqnum > ?",
+			level_brief.Seqnum)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		_, err = db.Query("UPDATE LevelsBrief SET is_active = false WHERE id = ?",
+			level_brief.ID)
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		panic("already deleted (archived)")
+	}
+
 }
 
 func connect_db() *sql.DB {
@@ -76,51 +144,12 @@ func connect_db() *sql.DB {
 	fmt.Println("Pass:", password)
 	db, err := sql.Open("mysql", fmt.Sprintf("db_user:%s@tcp(0.0.0.0:8089)/levels", password))
 
-	// if there is an error opening the connection, handle it
 	if err != nil {
 		panic(err.Error())
 	}
 
 	fmt.Println("DB Connected")
-
-	// defer the close till after the main function has finished
-	// executing
 	return db
-}
-
-func init_db() {
-	fmt.Println("DB Connecting")
-	password := os.Getenv("MYSQL_PASS")
-	fmt.Println("Pass:", password)
-	db, err := sql.Open("mysql", fmt.Sprintf("db_user:%s@tcp(0.0.0.0:8089)/levels", password))
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Println("DB Connected")
-
-	// defer the close till after the main function has finished
-	// executing
-	defer db.Close()
-
-	// Execute the query
-	results, err := db.Query("SELECT id, nickname FROM Users")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-
-	for results.Next() {
-		var user User
-		// for each row, scan the result into our tag composite object
-		err = results.Scan(&user.ID, &user.Nickname)
-		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
-		}
-		// and then print out the tag's Name attribute
-		fmt.Println(user.ID, user.Nickname)
-	}
 }
 
 func main() {
@@ -131,13 +160,16 @@ func main() {
 		r := mux.NewRouter().StrictSlash(true)
 
 		// level := LevelsBrief{0, 1, 2, 3, true, "Level1", "Test level"}
-		// level2 := LevelsBrief{0, 1, 2, 3, true, "Level1", ""}
-		// level2.create()
+		level2 := LevelsBrief{0, 1, 5, 3, true, "Level221", "Test descr"}
+		level2.create()
 
-		var lvl_br LevelsBrief
-		lvl_br.read(2)
+		// var lvl_br LevelsBrief
+		// lvl_br.read(2)
 
-		fmt.Println("Lvl name: ", lvl_br.Name)
+		// fmt.Println("Lvl name: ", lvl_br.Name)
+
+		level := LevelsBrief{5, 1, 0, 3, true, "Level1111", "Test level"}
+		level.delete()
 
 		exec.Command("fuser", "-k", "8082/tcp").Output()
 		http.ListenAndServe(":8082", r)
