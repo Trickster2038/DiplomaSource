b512342e44ffa93b358e141e53a65437b9ee1e89
Astakhov Sergey <fzastahov@gmail.com>
2023-01-16 14:35:47 +0300

    updating test db in work

--- mysql-db/db_dump.sql
+++ mysql-db/db_dump.sql
@@ -1,8 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
---
--- Host: 0.0.0.0    Database: levels
--- ------------------------------------------------------
--- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,10 +10,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `LevelsBrief`
---
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `levels` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
+USE `levels`;
 DROP TABLE IF EXISTS `LevelsBrief`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -34,20 +28,15 @@ CREATE TABLE `LevelsBrief` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `LevelsBrief`
---
-
 LOCK TABLES `LevelsBrief` WRITE;
 /*!40000 ALTER TABLE `LevelsBrief` DISABLE KEYS */;
-INSERT INTO `LevelsBrief` VALUES (1,1,2,3,1,'Test level 123','Description there');
+INSERT INTO `LevelsBrief` VALUES 
+(1,4,1,10,1,'Device lvl 1','Device test lvl');
+(1,1,2,0,1,'Text lvl 1','Text block');
+(1,2,3,5,1,'Single lvl 1','Singlechoice test');
+(1,3,4,7,1,'Multi lvl 1','Multichoice test');
 /*!40000 ALTER TABLE `LevelsBrief` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `LevelsData`
---
-
 DROP TABLE IF EXISTS `LevelsData`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -58,26 +47,20 @@ CREATE TABLE `LevelsData` (
   `question` text NOT NULL,
   `answer` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `LevelsData`
---
 
 LOCK TABLES `LevelsData` WRITE;
 /*!40000 ALTER TABLE `LevelsData` DISABLE KEYS */;
-INSERT INTO `LevelsData` VALUES (1,'Wide desc','1field','2filed','3field');
+INSERT INTO `LevelsData` VALUES 
+(1,'This is wide desr xxx','Code example','module adder_tb;  \n// Inputs  \nreg [3:0] A;  \nreg [3:0] B;  \nreg Cin;  \n// Outputs  \nwire [3:0] Sum;  \nwire Cout;  \n// Instantiate the Unit Under Test (UUT)  \nripple_adder_4bit uut (  \n.Sum(Sum),  \n.Cout(Cout),  \n.A(A),  \n.B(B),  \n.Cin(Cin)  \n);  \ninitial begin  \n// Initialize Inputs  \nA = 0;  \nB = 0;  \nCin = 0;  \n// Wait 100 ns for global reset to finish  \n#100;  \n// Add stimulus here  \nA=4\'b0001;B=4\'b0000;Cin=1\'b0;  \n#10 A=4\'b1010;B=4\'b0011;Cin=1\'b0;  \n#10 A=4\'b1101;B=4\'b1010;Cin=1\'b1;  \nend  \ninitial begin  \n$dumpfile(\"adder.vcd\");  \n$dumpvars;  \nend  \nendmodule\n','[{\"data\":[\"b0\",\"b1\",\"b1101\",\"b1000\"],\"name\":\"Sum[0:3]\",\"wave\":\"=...................=.=.=.....\"},{\"data\":[],\"name\":\"Cout\",\"wave\":\"0.......................1.....\"},{\"data\":[\"b0\",\"b1\",\"b1010\",\"b1101\"],\"name\":\"A[0:3]\",\"wave\":\"=...................=.=.=.....\"},{\"data\":[\"b0\",\"b11\",\"b1010\"],\"name\":\"B[0:3]\",\"wave\":\"=.....................=.=.....\"},{\"data\":[],\"name\":\"Cin\",\"wave\":\"0.......................1.....\"}]'),
+(2,'Wide desc','1field','2filed','3field');
 /*!40000 ALTER TABLE `LevelsData` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `SolutionEfforts`
---
-
 DROP TABLE IF EXISTS `SolutionEfforts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `SolutionEfforts` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -88,20 +71,11 @@ CREATE TABLE `SolutionEfforts` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `SolutionEfforts`
---
-
 LOCK TABLES `SolutionEfforts` WRITE;
 /*!40000 ALTER TABLE `SolutionEfforts` DISABLE KEYS */;
 INSERT INTO `SolutionEfforts` VALUES (1,1,2,1,'2021-01-10 14:53:01'),(2,3,4,0,'2023-01-10 14:53:01');
 /*!40000 ALTER TABLE `SolutionEfforts` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `Types`
---
-
 DROP TABLE IF EXISTS `Types`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -109,23 +83,14 @@ CREATE TABLE `Types` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` char(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Types`
---
 
 LOCK TABLES `Types` WRITE;
 /*!40000 ALTER TABLE `Types` DISABLE KEYS */;
 INSERT INTO `Types` VALUES (1,'text'),(2,'singlechoice_test'),(3,'multichoice_test'),(4,'program');
 /*!40000 ALTER TABLE `Types` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `Users`
---
-
 DROP TABLE IF EXISTS `Users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -137,13 +102,9 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Users`
---
-
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'Deni', 1),(2,'David', 0),(3,'Mark', 0);
+INSERT INTO `Users` VALUES (1,'Deni',1),(2,'David',0),(3,'Mark',0);
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -155,5 +116,3 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-01-10 18:01:18
