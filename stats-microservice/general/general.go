fc3672331a1cbd82d3a810ad1d68f668654b9dfe
Astakhov Sergey <fzastahov@gmail.com>
2023-01-30 14:26:44 +0300

    Stats.general in work

---
+++ stats-microservice/general/general.go
@@ -0,0 +1,172 @@
+package general
+
+import (
+	"fmt"
+	"stats/connection"
+)
+
+// select lb.id, lb.name, lb.seqnum, IFNULL(sum(is_successful), 0) as solutions from (SELECT * FROM LevelsBrief WHERE is_active = 1) lb LEFT JOIN SolutionEfforts se on lb.id = se.level_id GROUP BY lb.id ORDER BY seqnum;
+
+type LevelPassed struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Seqnum    int    `json:"seqnum"`
+	Solutions int    `json:"solutions"`
+}
+
+type AvgEfforts struct {
+	ID         int     `json:"id"`
+	Name       string  `json:"name"`
+	Seqnum     int     `json:"seqnum"`
+	AvgEfforts float64 `json:"avg_efforts"`
+}
+
+type SolutionCount struct {
+	SolutionsCount int `json:"solutions_count"`
+	LevelsCount    int `json:"levels_count"`
+}
+
+type ActivtyByMonth struct {
+	Year           int `json:"year"`
+	Month          int `json:"month"`
+	SolutionsCount int `json:"solutions_count"`
+	EffortsCount   int `json:"efforts_count"`
+}
+
+func Each_level_passed() []LevelPassed {
+	var res []LevelPassed
+	db := connection.Connect_db()
+
+	results, err := db.Query("SELECT lb.id, lb.name, lb.seqnum, IFNULL(sum(is_successful), 0) as solutions " +
+		"FROM (SELECT * FROM LevelsBrief WHERE is_active = 1) lb " +
+		"LEFT JOIN SolutionEfforts se on lb.id = se.level_id " +
+		"GROUP BY lb.id ORDER BY seqnum")
+	if err != nil {
+		panic(fmt.Sprintf("Getting levels statuses error in DB:", err.Error()))
+	}
+
+	var r LevelPassed
+
+	for results.Next() {
+		err = results.Scan(
+			&r.ID,
+			&r.Name,
+			&r.Seqnum,
+			&r.Solutions)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		res = append(res, r)
+	}
+
+	defer db.Close()
+	return res
+}
+
+func Each_level_avg_efforts() []AvgEfforts {
+	var res []AvgEfforts
+	db := connection.Connect_db()
+
+	results, err := db.Query("SELECT lb.id, lb.name, lb.seqnum, IFNULL(IFNULL(sum(is_successful IS NOT NULL), 0)/IFNULL(sum(is_successful), 0), 0) as solutions " +
+		"FROM (SELECT * FROM LevelsBrief WHERE is_active = 1) lb " +
+		"LEFT JOIN SolutionEfforts se on lb.id = se.level_id " +
+		"GROUP BY lb.id ORDER BY seqnum")
+	if err != nil {
+		panic(fmt.Sprintf("Getting levels avg efforts error in DB:", err.Error()))
+	}
+
+	var r AvgEfforts
+
+	for results.Next() {
+		err = results.Scan(
+			&r.ID,
+			&r.Name,
+			&r.Seqnum,
+			&r.AvgEfforts)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		res = append(res, r)
+	}
+
+	defer db.Close()
+	return res
+}
+
+func Solutions_count_distribution() []SolutionCount {
+	var res []SolutionCount
+	db := connection.Connect_db()
+
+	results, err := db.Query("SELECT solutions, count(*) FROM (SELECT lb.id, lb.name, lb.seqnum, IFNULL(sum(is_successful), 0) as solutions " +
+		"FROM (SELECT * FROM LevelsBrief WHERE is_active = 1) lb " +
+		"LEFT JOIN SolutionEfforts se on lb.id = se.level_id " +
+		"GROUP BY lb.id ORDER BY seqnum) tb " +
+		"GROUP BY solutions ORDER BY solutions")
+	if err != nil {
+		panic(fmt.Sprintf("Getting solutions distribution error in DB:", err.Error()))
+	}
+
+	var r SolutionCount
+
+	for results.Next() {
+		err = results.Scan(
+			&r.SolutionsCount,
+			&r.LevelsCount)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		res = append(res, r)
+	}
+
+	defer db.Close()
+	return res
+}
+
+func Activity_by_month() []ActivtyByMonth {
+	var res []ActivtyByMonth
+	db := connection.Connect_db()
+
+	results, err := db.Query("Select YEAR(time), MONTH(time), IFNULL(sum(is_successful), 0) " +
+		"FROM SolutionEfforts GROUP BY MONTH(time), YEAR(time) " +
+		"ORDER BY YEAR(time), MONTH(time);")
+	if err != nil {
+		panic(fmt.Sprintf("Getting activity by month error in DB:", err.Error()))
+	}
+
+	var r ActivtyByMonth
+
+	for results.Next() {
+		err = results.Scan(
+			&r.Year, &r.Month, &r.SolutionsCount)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		res = append(res, r)
+	}
+
+	i := 0
+
+	results, err = db.Query("Select IFNULL(count(*), 0) " +
+		"FROM SolutionEfforts GROUP BY MONTH(time), YEAR(time) " +
+		"ORDER BY YEAR(time), MONTH(time);")
+	if err != nil {
+		panic(fmt.Sprintf("Getting activity by month error in DB:", err.Error()))
+	}
+
+	for results.Next() {
+		err = results.Scan(
+			&res[i].EffortsCount)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		i++
+	}
+
+	defer db.Close()
+	return res
+}
--- stats-microservice/main.go
+++ stats-microservice/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os/exec"
 	"runtime"
+	"stats/general"
 	"stats/personal"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -15,6 +16,10 @@ import (
 
 type PersonalStatsRequest struct {
 	UserID   int    `json:"user_id"`
+	StatType string `json:"stat_type"`
+}
+
+type GeneralStatsRequest struct {
 	StatType string `json:"stat_type"`
 }
 
@@ -63,6 +68,42 @@ func personal_stats(w http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(w).Encode(response)
 }
 
+func general_stats(w http.ResponseWriter, req *http.Request) {
+	var reqFrame GeneralStatsRequest
+	var response ResponseFrame
+
+	defer func() {
+		if panicInfo := recover(); panicInfo != nil {
+			response.StatusStr = "error"
+			response.StatusCode = 400
+			response.Message = fmt.Sprintf("Top-level panic: %v", panicInfo)
+			w.WriteHeader(response.StatusCode)
+			json.NewEncoder(w).Encode(response)
+		}
+	}()
+
+	reqBody, _ := ioutil.ReadAll(req.Body)
+	json.Unmarshal(reqBody, &reqFrame)
+
+	if reqFrame.StatType == "each_level_passed" {
+		response.Data = general.Each_level_passed()
+	} else if reqFrame.StatType == "each_level_avg_efforts" {
+		response.Data = general.Each_level_avg_efforts()
+	} else if reqFrame.StatType == "solutions_distribution" {
+		response.Data = general.Solutions_count_distribution()
+	} else if reqFrame.StatType == "activity_by_month" {
+		response.Data = general.Activity_by_month()
+	} else {
+		panic("Unknown general stat type")
+	}
+
+	response.StatusStr = "ok"
+	response.StatusCode = 200
+	response.Message = ""
+	w.WriteHeader(response.StatusCode)
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
 	if runtime.GOOS == "windows" {
 		fmt.Println("Can't Execute this on a windows machine")
@@ -70,6 +111,7 @@ func main() {
 		fmt.Println("Server start")
 		r := mux.NewRouter().StrictSlash(true)
 		r.HandleFunc("/personalstats", personal_stats).Methods("POST")
+		r.HandleFunc("/generalstats", general_stats).Methods("POST")
 
 		exec.Command("fuser", "-k", "8085/tcp").Output()
 		http.ListenAndServe(":8085", r)
