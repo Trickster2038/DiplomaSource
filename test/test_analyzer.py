385681a84603c210b644571b310252994381a0da
Astakhov Sergey <fzastahov@gmail.com>
2023-02-28 20:35:58 +0300

    Autotests - init

--- test/test_analyzer.py
+++ test/test_analyzer.py
@@ -2,7 +2,7 @@ import utils.settings as settings
 import utils.utils as utils
 from utils.consts import *
 
-def test_correct_negative():
+def test_correct_positive():
     resp = utils.send_request(settings.ANALYZER_PORT, \
         "check", Analyzer.single_valid_positive)
     assert utils.is_ok_response(resp)
