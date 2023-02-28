71e4c97c4956811e1bc0e272bec749c76ed35cc2
Astakhov Sergey <fzastahov@gmail.com>
2023-02-28 21:53:23 +0300

    allure integration

--- test/test_analyzer.py
+++ test/test_analyzer.py
@@ -2,7 +2,10 @@ import utils.settings as settings
 import utils.utils as utils
 from utils.consts import *
 
-def test_correct_positive():
+import allure
+
+@allure.description("Test for singlechoice right-answered task")
+def test_single_correct_positive():
     resp = utils.send_request(settings.ANALYZER_PORT, \
         "check", Analyzer.single_valid_positive)
     assert utils.is_ok_response(resp)
--- test/utils/utils.py
+++ test/utils/utils.py
@@ -1,10 +1,19 @@
 import requests
+import allure
+import json
 
+@allure.step("Send request")
 def send_request(port, url, payload):
-    resp = requests.post(f"http://127.0.0.1:{port}/{url}", json = payload)
+    path = f"http://127.0.0.1:{port}/{url}"
+    allure.attach(path, 'Request URL', allure.attachment_type.TEXT)
+    allure.attach(json.dumps(payload, indent=4).encode(), 'Request payload', allure.attachment_type.TEXT)
+    resp = requests.post(path, json = payload)
+    allure.attach(json.dumps(resp.json(), indent=4).encode(), 'Response payload', allure.attachment_type.TEXT)
     return resp
 
+@allure.step("Check response [ok]")
 def is_ok_response(response):
+    allure.attach(json.dumps(response.json(), indent=4).encode(), 'Response payload', allure.attachment_type.TEXT)
     return response.status_code == 200 \
         and response.json()["status_code"] == 200 \
             and response.json()["status_str"] == "ok"
