703f3ec4e9dd9363b77c39e6ece583d038da59c8
Astakhov Sergey <fzastahov@gmail.com>
2023-03-01 15:35:15 +0300

    test - wavedrom - done

--- test/test_wavedrom.py
+++ test/test_wavedrom.py
@@ -6,6 +6,15 @@ import allure
 import copy
 import json
 
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
+
+
 @allure.description("Test for valid diagram")
 @allure.epic("Unit-testing")
 @allure.story("Wavedrom")
@@ -13,7 +22,8 @@ def test_wavedrom_correct_positive():
     resp = utils.send_request(settings.WAVEDROM_PORT,
                               "wavedrom", Wavedrom.valid_request)
     assert utils.is_ok_response(resp)
-    assert json.dumps(resp.json()) == json.dumps(Wavedrom.valid_response)
+    assert ordered(resp.json()) == ordered(Wavedrom.valid_response)
+
 
 @allure.description("Test for invalid diagram")
 @allure.epic("Unit-testing")
